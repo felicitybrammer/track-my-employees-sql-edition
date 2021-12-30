d85ee72efaee7d5d75d2ff10024a596589891065
Felicity Brammer <felicitybrammer@gmail.com>
2021-12-29 20:25:59 -0700

    dotenv variable and index fns

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
         host: 'localhost',
         user: process.env.DB_USER,
         password: process.env.DB_PW,
-        database: 'company'
+        database: process.env.DB_NAME
     },
     console.log('Connected to Database')
 ).promise();
